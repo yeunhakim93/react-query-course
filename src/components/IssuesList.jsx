7091cb75ec5e9e3c5dd4262cf54729e73f5602c5
Yeunha Kim <yeunhakim93@gmail.com>
2023-07-20 22:12:07 -0700

    Separated out IssueItem

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,51 +1,5 @@
-import { Link } from "react-router-dom";
 import { useQuery } from "react-query";
-import { GoIssueOpened, GoIssueClosed, GoComment } from "react-icons/go";
-import { relativeDate } from "../helpers/relativeDate";
-
-function IssueItem({ issue }) {
-  const {
-    title,
-    number,
-    assignee,
-    comments,
-    createdBy,
-    createdDate,
-    labels,
-    status,
-  } = issue;
-  const commentCount = comments.length;
-  return (
-    <li>
-      {status === "done" || status === "cancelled" ? (
-        <GoIssueClosed style={{ color: "red" }} />
-      ) : (
-        <GoIssueOpened style={{ color: "green" }} />
-      )}
-      <div className="issue-content">
-        <span>
-          <Link to={`/issue/${number}`}>{title}</Link>
-          {labels.map((label) => (
-            <span key={label} className={`label red`}>
-              {label}
-            </span>
-          ))}
-        </span>
-        <small>
-          #{number} opened {relativeDate(createdDate)} by {createdBy}
-        </small>
-      </div>
-      {assignee && <div>{assignee}</div>}
-      <span className="comment-count">
-        {commentCount > 0 && (
-          <>
-            <GoComment /> {commentCount}
-          </>
-        )}
-      </span>
-    </li>
-  );
-}
+import IssueItem from "./IssueItem";
 
 export default function IssuesList() {
   const issuesQuery = useQuery(["issues"], () =>
