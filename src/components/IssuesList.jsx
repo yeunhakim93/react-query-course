2d22be8558e09336d13753c5290f790876f4f8af
Yeunha Kim <yeunhakim93@gmail.com>
2023-08-09 14:56:49 -0700

    QUERY CLIENT DEFAULTS

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -3,17 +3,11 @@ import { useQuery } from "react-query";
 import IssueItem from "./IssueItem";
 import { fetchWithError } from "../helpers/fetchWithError";
 export default function IssuesList({ labels, status }) {
-  const issuesQuery = useQuery(
-    ["issues", { labels, status }],
-    () => {
-      const statusString = status ? `&status=${status}` : "";
-      const labelsStr = labels?.map((label) => `labels[]=${label}`).join("&");
-      return fetchWithError(`/api/issues?${labelsStr}${statusString}`);
-    },
-    {
-      staleTime: 1000 * 60,
-    }
-  );
+  const issuesQuery = useQuery(["issues", { labels, status }], () => {
+    const statusString = status ? `&status=${status}` : "";
+    const labelsString = labels.map((label) => `labels[]=${label}`).join("&");
+    return fetchWithError(`/api/issues?${labelsString}${statusString}`);
+  });
 
   const [searchValue, setSearchValue] = useState("");
   const searchQuery = useQuery(
